51d4630611597a7761dcde54a3bd4c4ac3a5e1f8
Andrew Watkins <andrew@groat.nz>
2020-08-13 17:06:25 +1200

    add tank data and map page

---
+++ data/tanks.js
@@ -0,0 +1,94 @@
+export const bays = [
+  {
+    rows: 20,
+    height_mm: 20000,
+    cols: 10,
+    width_mm: 10000,
+  }
+]
+export const tanks = [
+  {
+    name: 'A1',
+    colour: 'green',
+    capacity_l: 10000,
+    diameter_mm: 2400,
+    address: { bay:0, row:2, col:2}
+  },
+  {
+    name: 'A2',
+    colour: 'green',
+    capacity_l: 10000,
+    diameter_mm: 2400,
+    address: { bay:0, row:2, col:5}
+  },
+  {
+    name: 'A2',
+    colour: 'green',
+    capacity_l: 10000,
+    diameter_mm: 2400,
+    address: { bay:0, row:2, col:8}
+  },
+  {
+    name: 'B1',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:5, col:1}
+  },
+  {
+    name: 'B2',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:5, col:3}
+  },
+  {
+    name: 'C1',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:7, col:1}
+  },
+  {
+    name: 'C2',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:7, col:3}
+  },
+  {
+    name: 'C1',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:7, col:1}
+  },
+  {
+    name: 'C2',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:7, col:3}
+  },
+  {
+    name: 'D1',
+    colour: 'orange',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:9, col:1}
+  },
+  {
+    name: 'D2',
+    colour: '#602060',
+    capacity_l: 5000,
+    diameter_mm: 1500,
+    address: { bay:0, row:9, col:3}
+  },
+  {
+    name: 'E1',
+    colour: '#202080',
+    capacity_l: 22000,
+    diameter_mm: 3000,
+    address: { bay:0, row:7, col:7}
+  }
+]
